b790ae00b44fbb8dff5703fa2d25a49dd2de7fd3
Tristan Gruener <tristan.gruener@gmail.com>
2020-04-26 20:51:48 -0700

    got initial combined output

--- src/components/CategoryBreakdown.js
+++ src/components/CategoryBreakdown.js
@@ -7,6 +7,7 @@ import Expense from './Expense'
 function CategoryBreakdown() {
   const { user, firebase } = React.useContext(FirebaseContext)
   const [expenses, setExpenses] = React.useState([])
+  const [combinedExpenses, setCombinedExpenses] = React.useState([])
 
   React.useEffect(() => {
     if (user) {
@@ -18,25 +19,62 @@ function CategoryBreakdown() {
     const month = moment(Date.now()).format('MMMM')
     const year = moment(Date.now()).format('YYYY')
 
-    // setMonth(month)
-    // setYear(year)
-
+    //sort firebase query by Category
     firebase.db
       .collection('expenses')
       .where("postedBy.id", "==", user.uid)
       // .where("monthCreated", "==", month)
       .where("yearCreated", "==", year)
+      .orderBy("category")
       .orderBy('created', 'desc')
       .onSnapshot(handleSnapshot)
   }
 
   function handleSnapshot(snapshot) {
-    const expenses = snapshot.docs.map(doc => {
+    //sorted by category
+    const defaultExpenses = snapshot.docs.map(doc => {
       return { id: doc.id, ...doc.data() }
     })
-    // setDefaultExpenses(expenses)
-    setExpenses(expenses)
-    // getTotal(expenses)
+    combineCategories(defaultExpenses)
+  }
+
+  function combineCategories(defaultExpenses) {
+
+    const combinedCategories = []
+    let indexOffset = 0
+
+    defaultExpenses.map((exp, i) => {
+      if (indexOffset === 0) {
+        combinedCategories.push(exp)
+
+        // console.log(combinedCategories[i].amount)
+        // combinedCategories[i].amount += 333
+        // console.log(combinedCategories[i].amount)
+        indexOffset++
+      }
+
+      if (indexOffset > 0) {
+
+        //you gotta stash the index somehow... don't let it increment
+        console.log(combinedCategories[indexOffset - 1].category)
+
+        if (combinedCategories[indexOffset - 1].category === exp.category) {
+          console.log('hit')
+          //do the add
+          console.log(combinedCategories[indexOffset - 1].amount)
+          combinedCategories[indexOffset - 1].amount += exp.amount
+          console.log(combinedCategories[indexOffset - 1].amount)
+        } else {
+          combinedCategories.push(exp)
+          indexOffset++
+        }
+
+        // combinedCategories.push(exp)
+      }
+    })
+
+    // console.log(combinedCategories)
+    setExpenses(combinedCategories)
   }
 
   return (
