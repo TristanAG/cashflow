dfa49147cf7322dea7177df68211e061289f6252
Tristan Gruener <tristan.gruener@gmail.com>
2021-03-14 12:33:39 -0700

    Updates

---
+++ precache-manifest.c91e674d3fac22baea34f35eb2014f38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bc568cbd91b433d050926c0e695d02",
+    "url": "/cashflow/index.html"
+  },
+  {
+    "revision": "e2cf942b25aac72b81af",
+    "url": "/cashflow/static/css/main.0e17da89.chunk.css"
+  },
+  {
+    "revision": "d05968569482393dfcf9",
+    "url": "/cashflow/static/js/2.1b9bb2a6.chunk.js"
+  },
+  {
+    "revision": "e2cf942b25aac72b81af",
+    "url": "/cashflow/static/js/main.2f4ce786.chunk.js"
+  },
+  {
+    "revision": "f5d5337df6d80262f70d",
+    "url": "/cashflow/static/js/runtime~main.4845eb3d.js"
+  }
+]);
